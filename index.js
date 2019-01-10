675d368a4c90927981f3439f5568ea4bfd6f5bbb
pistachiyoda <fujisawamai21@gmail.com>
2019-01-10 22:00:26 +0900

    ＋するあたいを２に変更

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const _ = require("lodash");
 
 exports.handler = function() {
     const array = [1,2,3];
-    console.log(_.map(array, item => item + 1));
+    console.log(_.map(array, item => item + 2));
 }
